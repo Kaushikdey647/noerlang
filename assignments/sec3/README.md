5f032277845bc15c3c5ba30d0ff5494cd36d54be
Kaushik Dey <Kaushikdey647@gmail.com>
2023-08-09 12:19:41 +0000

    Added new tests maybe?

---
+++ assignments/sec3/README.md
@@ -0,0 +1,4 @@
+## How to run
+- `flex spec.l`
+- `gcc -o checker lex.yy.c -lfl`
+- `./checker test.c`
---
+++ tests/lexer_practice/sec3/test.c
@@ -0,0 +1,15 @@
+main()
+{
+    int i=0;
+    int total=0;
+    float count;
+    char ch;
+    input(count);
+    for(i=0;i<10;i++)
+    {
+        input(x);
+        total= total+x;
+    }
+    
+    display(total);
+}
